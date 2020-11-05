6d4c90f138f8a7cdfad8a4f63df82a1e8cb122cc
Niki Catapano <nsc9605@gmail.com>
2020-11-04 19:13:45 -0500

    created array for characters

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Special characters for the function created
-const specialCharacters = "!@#$%^&*()";
-const generateButton = document.getElementById('generateBtn')
-generateButton.addEventListener('click', writePassword)
+// Create an array for the characters options to choose from for password
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCasedCharacters = lowerCasedCharacters.map(x => x.toUpperCase());
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Write password to the #password input
 function writePassword() {
@@ -16,15 +16,5 @@ function writePassword() {
 
 }
 
-// Prompts that come up after you click generate password
-function generatePassword() {
-  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
-
-  var lowerCases = confirm("Do you want lowercases in your password?");
-
-  var upperCases = confirm("Do you want uppercases in your password?");
-
-  var numbers = confirm("Do you want numbers in your password?");
-
-  var special = confirm("Do you want special characters in your password?");
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
